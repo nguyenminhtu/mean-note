b29dee64962d9977407391835b16112911c28da7
Nguyen Tu <tuunguyen2795@gmail.com>
2016-12-08 17:02:21 +0700

    Updated edit and sign up features

--- public/controllers/HomeCtrl.controller.js
+++ public/controllers/HomeCtrl.controller.js
@@ -1,6 +1,6 @@
 angular.module('meannote')
 
-    .controller('HomeCtrl', ['$scope', '$http', '$location', '$rootScope', '$cookies', 'toastr', function ($scope, $http, $location, $rootScope, $cookies, toastr) {
+    .controller('HomeCtrl', ['$scope', '$http', '$location', '$rootScope', '$cookies', 'toastr', function($scope, $http, $location, $rootScope, $cookies, toastr) {
 
         if (!$rootScope.currentUser || !$rootScope.token) {
             $location.path('/');
@@ -8,29 +8,34 @@ angular.module('meannote')
 
         function getNote() {
             $http.get('/api/notes',
-                {headers: {
+                {
+                    headers: {
                         'author': $rootScope.token
                     }
-                }).success(function (data) {
+                }).success(function(data) {
                     $scope.notes = data;
                 });
         }
 
-        $scope.addNote = function () {
+        $scope.addNote = function() {
             $http.post('/api/notes/add',
                 { text: $scope.newNote },
-                {headers: {
+                {
+                    headers: {
                         'author': $rootScope.token
                     }
-                }).success(function (data) {
+                }).success(function(data) {
+                    toastr.success('Note added.', 'Success', {
+                        closeButton: true
+                    });
                     getNote();
                     $scope.newNote = '';
                 });
         }
 
-        $scope.removeNote = function (note) {
+        $scope.removeNote = function(note) {
             if (confirm("Are you sure want to delete this note ?")) {
-                $http.post('/api/notes/remove', { note: note }).success(function (data) {
+                $http.post('/api/notes/remove', { note: note }).success(function(data) {
                     toastr.success('Note deleted.', 'Success', {
                         closeButton: true
                     });
@@ -39,9 +44,23 @@ angular.module('meannote')
             }
         }
 
+        $scope.saveNote = function(note) {
+            var id = note._id;
+            var text = document.getElementById(id).value;
+            $http.post('/api/notes/edit/' + note._id, { text: text }).success(function(data) {
+                if (data === 'ok') {
+                    $scope.editMode = false;
+                    toastr.success('Note updated.', 'Success', {
+                        closeButton: true
+                    });
+                    getNote();
+                }
+            });
+        }
+
         getNote();
 
-        $scope.logout = function () {
+        $scope.logout = function() {
             $cookies.remove('token');
             $cookies.remove('currentUser');
             $rootScope.token = null;
--- routes/notes.js
+++ routes/notes.js
@@ -5,34 +5,48 @@ var JWT_SECRET = 'tuhandsome';
 
 Note = require('../models/note');
 
-router.get('/', function (req, res, next) {
-	var token = req.headers.author;
-	var user = jwt.decode(token, JWT_SECRET);
+router.get('/', function(req, res, next) {
+    var token = req.headers.author;
+    var user = jwt.decode(token, JWT_SECRET);
 
-	Note.find({ username: user.username }, function (err, notes) {
-		res.json(notes);
-	});
+    Note.find({ username: user.username }, function(err, notes) {
+        res.json(notes);
+    });
 });
 
-router.post('/add', function (req, res) {
-	var token = req.headers.author;
-	var user = jwt.decode(token, JWT_SECRET);
+router.post('/add', function(req, res) {
+    var token = req.headers.author;
+    var user = jwt.decode(token, JWT_SECRET);
 
-	var note = new Note({
-		text: req.body.text,
-		user: user._id,
-		username: user.username
-	});
+    var note = new Note({
+        text: req.body.text,
+        user: user._id,
+        username: user.username
+    });
 
-	note.save(function (err, result) {
-		res.json('ok');
-	});
+    note.save(function(err, result) {
+        res.json('ok');
+    });
 });
 
-router.post('/remove', function (req, res) {
-	Note.findOneAndRemove({ _id: req.body.note._id }, function (err, result) {
-		res.json('ok');
-	});
+router.post('/remove', function(req, res) {
+    Note.findOneAndRemove({ _id: req.body.note._id }, function(err, result) {
+        res.json('ok');
+    });
+});
+
+router.post('/edit/:id', function(req, res) {
+    var id = req.params.id;
+    Note.findOneAndUpdate({ _id: id },
+        {$set: {text: req.body.text}},
+        { safe: true, upsert: true },
+        function(err, result) {
+			if(err) {
+				console.log(err);
+			}else{
+				res.json('ok');
+			}
+        });
 });
 
 module.exports = router;
